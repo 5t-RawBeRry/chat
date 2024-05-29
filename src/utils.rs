94ab0cc7514f1017127a9e093aa38796eebd6ae7
K <hs_err@msn.com>
2024-05-29 17:45:38 +0800

    修复Tripcode 处理不正确的bug

--- src/utils.rs
+++ src/utils.rs
@@ -9,11 +9,11 @@ pub type Cache = Arc<Mutex<HashMap<String, Vec<Message>>>>;
 pub fn process_username(username: &str) -> String {
     if let Some(pos) = username.find('#') {
         let (nick, tripcode) = username.split_at(pos);
-        if !nick.is_empty() {
-            format!("{}#{}", nick, Fourchan::generate(tripcode))
-        } else {
-            username.to_string()
+        if nick.is_empty() {
+            return username.to_string();
         }
+        let tripcode_without_hash = &tripcode[1..];
+        format!("{}#{}", nick, Fourchan::generate(tripcode_without_hash))
     } else {
         username.to_string()
     }
